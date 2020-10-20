f2395e72955a6c6dd496cda1967a087677e6e0df
a-mccaffrey <aisling.s.mccaffrey@outlook.com>
2020-10-19 21:49:27 -0400

    Updates

---
+++ precache-manifest.df75f778b681032328e66b3b4995d2cd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9011a4af889fb4d3dc599ceae4ddf4b1",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "eada50dcde7d6679dbe9",
+    "url": "/react-portfolio/static/css/2.811d5a83.chunk.css"
+  },
+  {
+    "revision": "aea57011c6e29db7214c",
+    "url": "/react-portfolio/static/css/main.777f084e.chunk.css"
+  },
+  {
+    "revision": "eada50dcde7d6679dbe9",
+    "url": "/react-portfolio/static/js/2.24e99001.chunk.js"
+  },
+  {
+    "revision": "a6a2137e57c7626f69a79e12867d16c1",
+    "url": "/react-portfolio/static/js/2.24e99001.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aea57011c6e29db7214c",
+    "url": "/react-portfolio/static/js/main.5d4221f1.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "bb7a5a72863efb6fe5967834a7d073d3",
+    "url": "/react-portfolio/static/media/Garden.bb7a5a72.png"
+  },
+  {
+    "revision": "dfaa2d7caed33c6df85be6103d70ec5c",
+    "url": "/react-portfolio/static/media/faded-pink-divider.dfaa2d7c.svg"
+  },
+  {
+    "revision": "d5268c27deeded54c811c24f421dd80c",
+    "url": "/react-portfolio/static/media/portfolio-animation.d5268c27.gif"
+  },
+  {
+    "revision": "050bf82ca8287a5923a796cb8f1a4485",
+    "url": "/react-portfolio/static/media/portfolio-coding copy.050bf82c.png"
+  },
+  {
+    "revision": "db37753f6fba81af7319df6624f05092",
+    "url": "/react-portfolio/static/media/portfolio-graphic-design.db37753f.png"
+  },
+  {
+    "revision": "122816d0760557e8385318572332e003",
+    "url": "/react-portfolio/static/media/portfolio-writing copy.122816d0.png"
+  }
+]);
